b141e4d98e420caabe1c53e39f9bc2cb009be02a
francisbouvier <5114029+francisbouvier@users.noreply.github.com>
2026-05-20 03:31:50 +0000

    lightpanda Formula 2026.05.20.032932

--- Formula/lightpanda.rb
+++ Formula/lightpanda.rb
@@ -1,7 +1,7 @@
 class Lightpanda < Formula
   desc "Headless browser for AI agents and automation (nightly build)"
   homepage "https://github.com/lightpanda-io/browser"
-  version "2026.05.19.033042"
+  version "2026.05.20.032932"
 
   livecheck do
     url "https://api.github.com/repos/lightpanda-io/browser/releases/tags/nightly"
@@ -13,7 +13,7 @@ class Lightpanda < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/lightpanda-io/browser/releases/download/nightly/lightpanda-aarch64-macos"
-      sha256 "f21142a29aac5b4beb44d171c149f58c88ebd2cc887780d500eaaeb9f38a921e"
+      sha256 "b245d4a1f65deebdc2352f2cc6cf31da42f4ea9dd815f88455ccb44744baf2d9"
     else
       url "https://github.com/lightpanda-io/browser/releases/download/nightly/lightpanda-x86_64-macos"
       sha256 "1aa48c6789349d0c747e9ca98ff3fb8c5a4e616716796649d5602f950e687d45"
